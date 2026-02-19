5c00f99b3241549c4f163f5a5787f2f3cfa42b38
Anka <runner@iad20-eo1209-d1f3b5df-36c9-4b3f-852e-7583c8fc77e7-861BBFF2A7FF.local>
2026-02-19 06:55:37 +0000

    mdns-browser 1.5.1

--- Casks/mdns-browser.rb
+++ Casks/mdns-browser.rb
@@ -1,7 +1,7 @@
 cask "mdns-browser" do
-  version "0.23.1"
+  version "1.5.1"
   # see https://github.com/hrzlgnm/mdns-browser/releases/download/mdns-browser-v#{version}/mdns-browser_#{version}_universal.dmg.sha256
-  sha256 "29d6f89eb3263cb1bacc9042ba6a98e27721fc0acb156dd7b2e47d3634864f8e"
+  sha256 "b0ac38e900ea1697e1ab1d4b975ff3d494c13fc75fe86c6941bc136c0d977034"
 
   url "https://github.com/hrzlgnm/mdns-browser/releases/download/mdns-browser-v#{version}/mdns-browser_#{version}_universal.dmg"
   name "mDNS-Browser"
